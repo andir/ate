eb6170663c88454650baf1fbb5d67c047440add3
Andreas Rammhold <andreas@rammhold.de>
2019-05-09 23:33:36 +0200

    complete renaming

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { stdenv, pkgs, pkgconfig, gnome3, gnumake }:
 stdenv.mkDerivation {
-  name = "at-0.0.0";
+  name = "ate-0.0.0";
 
   buildInputs = [ gnome3.vte or pkgs.vte ];
   nativeBuildInputs = [ pkgconfig gnumake pkgs.gdb pkgs.clang-tools ];
@@ -9,9 +9,9 @@ stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp at $out/bin
+    cp ate $out/bin
 
     # for compat
-    ln -s $out/bin/at $out/bin/st
+    ln -s $out/bin/ate $out/bin/st
   '';
 }
