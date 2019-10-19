d73e5dafd949140bbb0bbae80b99e0c646b5baff
Malte Brandy <malte.brandy@maralorn.de>
2019-10-19 22:07:48 +0200

    Add keybindings to test

--- test.nix
+++ test.nix
@@ -1,7 +1,11 @@
-{ nixpkgs ? import <nixpkgs> {}}:
-let
-  mkTest = config: nixpkgs.callPackage ./default.nix { config.ate = config; };
-in
-{
-  default = mkTest {};
+{ nixpkgs ? import <nixpkgs> { } }:
+let mkTest = config: nixpkgs.callPackage ./default.nix { config.ate = config; };
+in {
+  default = mkTest {
+    keybindings = {
+      # INCREMENT_FONT = "underscore"; Not supported!
+      DECREMENT_FONT = "mod1+bla";
+      PIPECMD = "control+shift+u";
+    };
+  };
 }
