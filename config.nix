405041079ca03e0abcf438dc7c0372750fcb48b9
Andreas Rammhold <andreas@rammhold.de>
2019-12-26 14:42:40 +0100

    MOve the config nix logic to `config.nix` to regain a bit more readability in the default.nix

---
+++ config.nix
@@ -0,0 +1,34 @@
+{ lib
+, keybindings ? {}
+, options ? {}
+}:
+let
+  # escape a value for usage in the CC argument list
+  mkEscapedValue = name: value:
+    let
+      # escape the value for the macro defintion
+      # int's and floats aren't quoted but converted to string instead
+      type = builtins.typeOf value;
+      v =
+        if lib.elem type [ "float" "int" ] then
+          toString value
+        else lib.escapeShellArg ''"${value}"'';
+    in v;
+
+  listOfKeybindings = lib.mapAttrsToList (key: value: let
+      keyList = lib.reverseList (lib.splitString "+" value);
+      modifiers = builtins.tail keyList;
+      keyValue = builtins.head keyList;
+    in {
+      "${key}_KEYVAL" = mkEscapedValue "" keyValue;
+      "${key}_MODIFIER_MASK" = ''"${lib.concatStringsSep " | " (map (m: "GDK_${lib.toUpper m}_MASK") modifiers)}"'';
+    }) keybindings;
+
+  keybindingDefines = lib.foldr (accu: val: accu // val) {} listOfKeybindings;
+  optionDefines = lib.mapAttrs mkEscapedValue options;
+
+  mkCFlag = key: value: "-D${key}=${value}";
+in
+  lib.concatStringsSep " " (
+    lib.mapAttrsToList mkCFlag (keybindingDefines // optionDefines)
+  )
--- default.nix
+++ default.nix
@@ -9,6 +9,17 @@
 , rofi
 , xurls
 }:
+let
+  pipecmd = pkgs.writeScript "pipecmd.sh" ''
+    #! ${stdenv.shell}
+    ${xurls}/bin/xurls | sort | uniq | ${rofi}/bin/rofi -dmenu | xargs -r firefox
+  '';
+
+  ateConfig = lib.recursiveUpdate {
+    options = { PIPECMD = toString pipecmd; BACKGROUND_OPACITY = 0.8; };
+    keybindings = {};
+  } (config.ate or {});
+in
 #
 # Pass config like
 #
@@ -36,37 +47,7 @@ stdenv.mkDerivation {
     src = lib.cleanSource ./.;
   };
 
-  CONFIG_CFLAGS = let
-    pipecmd = pkgs.writeScript "pipecmd.sh" ''
-      #! /bin/sh
-      ${xurls}/bin/xurls | sort | uniq | ${rofi}/bin/rofi -dmenu | xargs -r firefox
-    '';
-    defaultConfig = { PIPECMD = toString pipecmd; BACKGROUND_OPACITY = 0.8; };
-    mkEscapedValue = name: value:
-      let
-        # escape the value for the macro defintion
-        # int's and floats aren't quoted but converted to string instead
-        type = builtins.typeOf value;
-        v =
-          if lib.elem type [ "float" "int" ] then
-            toString value
-          else lib.escapeShellArg ''"${value}"'';
-      in v;
-
-    stringDefines = lib.mapAttrs mkEscapedValue (defaultConfig // (config.ate.options or {}));
-    keybindings = (config.ate.keybindings or {});
-    listOfKeybindings = lib.mapAttrsToList (key: value: let
-      keyList = lib.reverseList (lib.splitString "+" value);
-      modifiers = builtins.tail keyList;
-      keyValue = builtins.head keyList;
-    in {
-      "${key}_KEYVAL" = mkEscapedValue "" keyValue;
-      "${key}_MODIFIER_MASK" = ''"${lib.concatStringsSep " | " (map (m: "GDK_${lib.toUpper m}_MASK") modifiers)}"'';
-    }) keybindings;
-    keybindingDefines = lib.foldr (accu: val: accu // val) {} listOfKeybindings;
-    mkCFlag = key: value: "-D${key}=${value}";
-    configFlags = lib.mapAttrsToList mkCFlag (keybindingDefines // stringDefines);
-  in lib.concatStringsSep " " configFlags;
+  CONFIG_CFLAGS = import ./config.nix ({ inherit lib; } // ateConfig);
 
   installPhase = ''
     mkdir -p $out/bin
